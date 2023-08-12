d41b312c17ab4867dcd65605ab03f5ea53d0b8d6
Paul Miskinis <9098698+pauliusmiskinis@users.noreply.github.com>
2023-08-11 23:46:51 -0400

    Add delete and storage_class options

--- backup-s3/run.sh
+++ backup-s3/run.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-KEY=`jq -r .awskey /data/options.json`
-SECRET=`jq -r .awssecret /data/options.json`
-BUCKET=`jq -r .bucketname /data/options.json`
+KEY=$(jq -r .awskey /data/options.json)
+SECRET=$(jq -r .awssecret /data/options.json)
+BUCKET=$(jq -r .bucketname /data/options.json)
+DELETE=$(jq -r .delete /data/options.json)  # Renamed from MIRROR
+STORAGE_CLASS=$(jq -r '.storage_class // "STANDARD"' /data/options.json)
 
 now="$(date +'%d/%m/%Y - %H:%M:%S')"
 
@@ -11,6 +13,10 @@ echo $now
 aws configure set aws_access_key_id $KEY
 aws configure set aws_secret_access_key $SECRET
 
-aws s3 sync /backup/ s3://$BUCKET/  --delete
+delete_option=""
+if [ "$DELETE" = "true" ]; then
+  delete_option="--delete"
+fi
 
+aws s3 sync $delete_option --storage-class $STORAGE_CLASS /backup/ s3://$BUCKET/
 echo "Done"
